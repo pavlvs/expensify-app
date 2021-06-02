18198bf05b7f3a5770535517c28175c048811941
pavlvs <pavlvsxavier@gmail.com>
2021-06-02 07:50:20 +0100

    Set up react router with routes

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+)
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+)
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component
+    </div>
+)
+
+const HelpPage = () => (
+    <div>
+        This is from my help component
+    </div>
+)
+
+const routes = (
+    <Router>
+        <div>
+            <Route path='/' component={ExpenseDashboardPage} exact={true} />
+            <Route path='/create' component={AddExpensePage} />
+            <Route path='/edit' component={EditExpensePage} />
+            <Route path='/help' component={HelpPage} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
