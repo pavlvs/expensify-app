2283cf22acfffee89adc290b683302aa1050c836
pavlvs <pavlvsxavier@gmail.com>
2021-07-31 15:34:55 +0100

    Render individual expenses

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
@@ -19,7 +20,7 @@ store.subscribe(() => {
 store.dispatch(addExpense(
     {
         description: 'Water Bill',
-        amount: 300,
+        amount: 3000,
         createdAt: 1000
     }
 ))
@@ -27,11 +28,21 @@ store.dispatch(addExpense(
 store.dispatch(addExpense(
     {
         description: 'Gas Bill',
-        amount: 400,
+        amount: 4500,
         createdAt: 5000
 
     }
 ))
-store.dispatch(setTextFilter('bill'))
+store.dispatch(setTextFilter('water'))
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'))
+}, 3000)
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
